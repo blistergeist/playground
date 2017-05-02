7411c51f875c43e25852f887d5a4c000002ce588
blistergeist <morgan.j.allison@gmail.com>
2017-05-02 16:08:01 -0400

    added two files
    beautifulsoup_practice.py and html_parsing.py

---
+++ beautifulsoup_practice.py
@@ -0,0 +1,41 @@
+# beautifulsoup_practice.py
+import urllib.request
+from bs4 import BeautifulSoup
+
+html = urllib.request.urlopen('http://money.cnn.com/2016/12/23/technology/consumer-reports-macbook-pro/index.html')
+
+soup = BeautifulSoup(html, 'lxml')
+
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+# # print(soup.find_all('p'))
+# # print(soup.find_all('a'))
+# print(soup.find(href='https://www.lendingtree.com'))
+# a = soup.find_all('a')
+# for tag in a:
+# 	print(tag.attrs)
+
+# for link in soup.find_all('a'):
+# 	print(link.get('href'))
+
+print(soup('script') == soup.find_all('script'))
+
+print(len(soup.find_all('script')), 'scripts nonono')
+print(len(soup.find_all('style')), 'styles nonono')
+for script in soup(['script']):
+	script.extract()
+print(len(soup.find_all('script')), 'scripts nonono')
+# iterTag = soup.html
+# for i in range(5):
+# 	iterTag = iterTag.next_element
+# 	print(iterTag.next_element)
+
+
+# figure out how to filter out only the valid usable text
+# this is generally found in <p> (paragraph) tags 
+# why do you take out scripts and styles?
+# what are scripts and styles? 
+# a script is just that, code that does something client-side
+# a style affects how text is displayed for a given tag (color, size, etc.) 
+# I may be able to use find_parents() and find_siblings() to find valid text
---
+++ html_parsing.py
@@ -0,0 +1,22 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'http://money.cnn.com/2016/12/23/technology/consumer-reports-macbook-pro/index.html'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'lxml')
+
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text)
